b95e86d3b997156dca39923115ce0c3440f3975c
Alan TSang <alan.tsang@madhead.com>
2021-02-11 21:59:29 +0800

    [DEV] Push before continue heroku push tut

--- index.js
+++ index.js
@@ -9,7 +9,8 @@
 // import dependencies
 const bodyParser = require('body-parser'),
       express = require('express'),
-      app = express().use(bodyParser.json());
+      app = express().use(bodyParser.json())
+;
 
 // webhook setup
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
